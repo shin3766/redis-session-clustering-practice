cdce599f3ab06d39a832abcb6b21883a0394d87f
Yoosup Shin <brian96shin@gmail.com>
2024-10-30 10:32:44 +0900

    git init

---
+++ src/main/java/com/example/redissessionclustering/RedisSessionClusteringApplication.java
@@ -0,0 +1,13 @@
+package com.example.redissessionclustering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSessionClusteringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSessionClusteringApplication.class, args);
+    }
+
+}
